9f527b2672aaf8af85022f463f0cbb76fc2a48e0
kaizhu94 <zhukai0803@gmail.com>
2021-02-01 12:02:04 -0500

    new ignore

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://breadowner:JdL5sv8fqWpxb6HD@cluster0.6v9vq.mongodb.net/MERNdb?retryWrites=true&w=majority',
-    //Make sure this is your own unique string
-    secretOrKey: 'TKp3u1LbuITxxh4su12Catsrrt0Fj4w7'
-}
