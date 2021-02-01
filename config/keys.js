ae2c8a6c91d90e719eb990cae96c73d741af6221
jane baik <janebaik@berkeley.edu>
2021-02-01 07:16:12 -0800

    keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:odNziqNvQKtrFppV@cluster0.dlzg7.mongodb.net/cluster0?retryWrites=true&w=majority',
+    //Make sure this is your own unique string
+    secretOrKey: 'TKp3u1LbuITxxh4su12Catsrrt0Fj4w7'
+}
+
