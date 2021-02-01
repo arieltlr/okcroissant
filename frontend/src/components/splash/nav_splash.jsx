1a08e5889c9d18846b39c7090b3bd1f7c9fd28a8
jane baik <janebaik@berkeley.edu>
2021-02-01 12:00:04 -0800

    setup

---
+++ frontend/src/components/splash/nav_splash.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './splash.css'
+
+
+// onclickk
+const NavSplash = () =>{
+    return (
+        <div>
+            <nav className="splash-nav">
+                <img src="" alt="okCrossiantLogo"/>
+                <button>Login</button>
+                <button>Sign Up</button>
+            </nav>
+        </div>
+    )
+}
+
+export default NavSplash;
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,13 +1,17 @@
 // src/components/main/main_page.js
 
 import React from 'react';
+import NavSplash from './nav_splash'
+import './splash.css'
+
 
 class SplashPage extends React.Component {
 
     render() {
         return (
             <div>
-                hello
+                <NavSplash/>
+                <h1>splashpage</h1>
             </div>
         );
     }
