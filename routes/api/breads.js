e762b14c27b66d32771ade40ad0aff77ebe51230
Monica Liang <monicaliang@rocketmail.com>
2021-02-02 15:45:33 -0500

    Completed bread backend

--- routes/api/breads.js
+++ routes/api/breads.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const { db } = require('../../models/Bread');
 const Bread = require('../../models/Bread');
 const validateBreadInput = require('../../validation/bread');
 
@@ -28,5 +29,49 @@ router.post('/',
         newBread.save().then(bread => res.json(bread));
     }   
 );
+
+router.get("/:bread_id", (req, res) => {
+    Bread.findById(req.params.bread_id)
+        .then(bread => res.json(bread))
+        .catch(err => 
+            res.status(400).json({ nobreadfound: 'No bread found with that id!' })
+        );
+});
+
+router.get("/matches", (req, res) => {
+    Bread.find({ 
+        thin: { $eq: req.body.thin },
+        wholewheat: { $eq: req.body.wholewheat }, 
+        savory: { $eq: req.body.savory },
+        filling: { $eq: req.body.filling }
+    })
+        .then(breads => res.json(breads));
+})
+
+router.get("/", (req, res) => {
+    Bread.aggregate(
+        [ { $sample: { size: 20 } } ]
+    )
+        .then( breads => res.json(breads));
+})
+
+router.patch('/:bread_id', (req, res) => {
+    const { errors, isValid } = validateBreadInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    Bread.findByIdAndUpdate(
+        req.params.bread_id,
+        req.body,
+        {new: true},
+
+        (err, bread) => {
+            if (err) return res.status(500).send(err);
+            return res.send(bread);
+        }
+    )
+})
 
 module.exports = router;
