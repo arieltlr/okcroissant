553a911be84b9ae7e2b830ea1cad8043a6006547
kaizhu94 <zhukai0803@gmail.com>
2021-02-01 21:04:16 -0500

    test2

--- routes/api/breadprofile.js
+++ routes/api/breadprofile.js
@@ -44,36 +44,7 @@ router.post('/',
 //                 return res.status(400).json(errors);
 //             }
 
-//             Breadprofile.findOne({user: req.params.user_id}).then(
-//                 breadprofile => { 
-//                 if(breadprofile){
-//                     // debugger
-//                     const newBreadprofile = new Breadprofile({
-//                         user: req.user.id,
-//                         thin: req.body.thin,
-//                         wholewheat: req.body.wholewheat,
-//                         filling: req.body.filling,
-//                         savory: req.body.savory
-//                       });
-//                     // debugger
-//                     // Breadprofile.deleteOne({'_id': ObjectId(breadprofile.id)})
-//                     Breadprofile.findByIdAndRemove( req.params.user_id, (err, profile) => {
-//                         // As always, handle any potential errors:
-//                         if (err) return res.status(500).send(err);
-//                         // We'll create a simple object to send back with a message and the id of the document that was removed
-//                         // You can really do this however you want, though.
-//                         const response = {
-//                             message: "Profile successfully deleted"
-//                         };
-//                         return res.status(200).send(response);
-//                     });
-//                     newBreadprofile.save().then(breadprofile => res.json(breadprofile));
-//                 }else{
-//                 return res.status(400).json({breadprofile: "Bread profile doesn't exist!"})
-//             }
-//         })
-    // }  
-// )
+
 router.patch('/:breadprofile_id', 
         passport.authenticate('jwt', { session: false }),
         (req, res) => {
