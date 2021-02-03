60e169b90355158f81bfeba13b07be8b6a86021f
ariel taylor <ariel.taylorr@gmail.com>
2021-02-02 16:53:58 -0800

    Profile component logic

--- frontend/src/components/profile/profile_show.jsx
+++ frontend/src/components/profile/profile_show.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 class ProfileShow extends React.Component {
@@ -8,10 +9,20 @@ class ProfileShow extends React.Component {
 
     render(){
         return (
-            <div>
-                <h1>
-                    Profile show!
-                </h1>
+            <div className="profile-container">
+                <div className="prof-items-container">
+                    <div className="name-container">
+                        <div className="profile-pic"></div>
+                        <p className="display-username">{this.props.user.username}</p>
+                    </div>
+                    <div className="user-info-container">
+                        <p className="user-info">{this.props.user.username}</p>
+                        <Link to="/createBreadprofile">Your Bread Profile</Link>
+                        <p className="bread-reviews">Your Bread Reviews</p>
+                    </div>
+
+                </div>
+                
             </div>
         )
     }
