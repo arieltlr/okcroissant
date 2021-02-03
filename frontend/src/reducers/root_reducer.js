f092bfeece6b33181bddd9a7b698eb17e527851c
Monica Liang <monicaliang@rocketmail.com>
2021-02-02 19:36:07 -0500

    Merge Conflicts

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,22 +3,14 @@ import session from './session_api_reducer';
 import errors from './errors_reducer';
 import modalReducer from './modal_reducer';
 import uiReducer from './ui_reducer';
-<<<<<<< HEAD
-import breadMatchesReducer from './bread_matches_reducer';
-=======
 import breadReducer from './bread_reducer';
->>>>>>> bread
 
 const RootReducer = combineReducers({
     session,
     errors,
     modal: modalReducer,
     ui: uiReducer,
-<<<<<<< HEAD
-    breadMatches: breadMatchesReducer
-=======
     bread: breadReducer
->>>>>>> bread
 });
 
 export default RootReducer;
