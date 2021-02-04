e5f6f6853d570bea424a1e5b95a70eb4b357dd4b
Monica Liang <monicaliang@rocketmail.com>
2021-02-04 11:30:49 -0500

    Frontend CSS

--- frontend/src/components/splash/footer.jsx
+++ frontend/src/components/splash/footer.jsx
@@ -1,5 +1,6 @@
-
 import React from 'react';
+import Git from '../../images/github.png';
+import Linkedin from '../../images/linkedin.png';
 
 
 // onclickk
@@ -7,31 +8,31 @@ const Footer = () => {
     return (
         <footer className="footer-master">
             <div className="column">
-                <h1>Kai</h1>
+                <h1>Kai Zhu</h1>
                 <ul className="footer-lst">
-                    <li><a href="">Github</a></li>
-                    <li><a href="">LinkedIn</a></li>
+                    <li><a href="https://github.com/kaizhu94"><img src={Git} /></a></li>
+                    <li><a href="https://www.linkedin.com/in/kaihua-zhu-177a041b1/"><img src={Linkedin} /></a></li>
                 </ul>
             </div>
             <div className="column">
-                <h1>Monica</h1>
+                <h1>Monica Liang</h1>
                 <ul className="footer-lst">
-                    <li><a href="">Github</a></li>
-                    <li><a href="">LinkedIn</a></li>
+                    <li><a href="https://github.com/lmonica97"><img src={Git} /></a></li>
+                    <li><a href="https://www.linkedin.com/in/monica-liang-a06510146/"><img src={Linkedin} /></a></li>
                 </ul>
             </div>
             <div className="column">
-                <h1>Ariel</h1>
+                <h1>Ariel Taylor</h1>
                 <ul className="footer-lst">
-                    <li><a href="">Github</a></li>
-                    <li><a href="">LinkedIn</a></li>
+                    <li><a href="https://github.com/arieltlr"><img src={Git} /></a></li>
+                    <li><a href="https://www.linkedin.com/in/taylorariel/"><img src={Linkedin} /></a></li>
                 </ul>
             </div>
             <div className="column">
                 <h1>Jane Baik</h1>
                 <ul className="footer-lst">
-                    <li><a href="">Github</a></li>
-                    <li><a href="">LinkedIn</a></li>
+                    <li><a href=""><img src={Git} /></a></li>
+                    <li><a href=""><img src={Linkedin} /></a></li>
                 </ul>
             </div>
         </footer>
