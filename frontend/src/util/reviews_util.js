af9fd1a75d93dd21fbafb683e2605faab66af314
ariel taylor <ariel.taylorr@gmail.com>
2021-02-03 16:24:21 -0800

    Write review util actions and thunk actions

---
+++ frontend/src/util/reviews_util.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const fetchAllReviews = () => {
+    return axios.get('/api/reviews/')
+}
+export const fetchBreadReviews = (breadId) => {
+    return axios.get(`/api/reviews/bread/${breadId}`)
+};
+export const fetchUsersReviews = (userId) => {
+    return axios.get(`/api/reviews/user/${userId}`)
+};
+
+export const createReview = (review) => {
+    return axios.post('/api/reviews/', review)
+}
+
+export const updateReview = (review) => {
+    return axios.patch(`/api/reviews/${review.id}`, review)
+};
+export const deleteReview = reviewId => {
+    return axios.delete(`/api/reviews/${reviewId}`)
+};
